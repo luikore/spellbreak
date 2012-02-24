211aea5cbf1cea9ae26b08bc2ae99225314ceef8
Zete Lui <ns@theplant.jp>
2012-02-24 21:35:36 +0800

    scan indent / dedent / samedent / notdent

--- utils/scanner.h
+++ utils/scanner.h
@@ -1,8 +1,10 @@
 #pragma once
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <stdint.h>
-#include "darray.h"
+#include "ccan/darray.h"
 #include "u.h"
 
 typedef struct {
@@ -10,52 +12,61 @@ typedef struct {
   uint32_t to;
 } CharRange;
 
-typedef darray(size_t) PosStack;
-typedef darray(uint32_t) ChStack;
+enum Indent {
+  NOTDENT, INDENT, SAMEDENT, DEDENT
+};
+
+typedef struct {
+  size_t pos;
+  uint32_t ch;
+  int last_indent;
+} State;
+
+typedef darray(State) StateStack;
 
 typedef struct {
   uint8_t* str;
   size_t size;
   size_t pos;
   uint32_t ch;
+  int last_indent;
   // backward tracing
-  PosStack pos_stack;
-  ChStack ch_stack;
+  StateStack state_stack;
 } Scanner;
 
 static Scanner scanner_init(char* str, size_t size) {
   Scanner s;
   s.pos = 0;
   s.size = size;
-  s.str = (char*)malloc(size + 1);
+  s.str = (uint8_t*)malloc(size + 1);
   s.ch = 0;
+  s.last_indent = -1;
   strncpy(s.str, str, size);
 
-  darray_init(s.pos_stack);
-  darray_realloc(s.pos_stack, 10);
-  darray_init(s.ch_stack);
-  darray_realloc(s.ch_stack, 10);
+  darray_init(s.state_stack);
+  darray_realloc(s.state_stack, 10);
 
   return s;
 }
 
 static void scanner_free(Scanner* sc) {
   free(sc->str);
-  darray_free(sc->pos_stack);
-  darray_free(sc->ch_stack);
+  darray_free(sc->state_stack);
 }
 
 static void scanner_push(Scanner* sc) {
-  darray_push(sc->pos_stack, sc->pos);
-  darray_push(sc->ch_stack, sc->ch);
+  State s = {sc->pos, sc->ch, sc->last_indent};
+  darray_push(sc->state_stack, s);
 }
 
 static void scanner_pop(Scanner* sc) {
-  sc->pos = darray_pop(sc->pos_stack);
-  sc->ch = darray_pop(sc->ch_stack);
+  State s = darray_pop(sc->state_stack);
+  sc->pos = s.pos;
+  sc->ch = s.ch;
+  sc->last_indent = s.last_indent;
 }
 
-static int scanner_eos(Scanner* sc) {
+static bool scanner_eos(Scanner* sc) {
   return sc->pos == sc->size;
 }
 
@@ -108,14 +119,39 @@ static int scanner_scan_range(Scanner* sc, CharRange* r, size_t size, int negati
   }
 }
 
-static int scanner_word_boundary(Scanner* sc, size_t pos) {
+static bool scanner_word_boundary(Scanner* sc, size_t pos) {
   char* p = (char*)(sc->str + pos);
   return (isalnum(p[0]) || p[0] == '_') ^ (isalnum(p[1]) || p[1] == '_');
 }
 
+static enum Indent scanner_scan_indent(Scanner* sc) {
+  if (!sc->pos) {
+    return SAMEDENT;
+  }
+  // backtrack ' ' until meet bol
+  size_t pos = sc->pos - 1;
+  char* p = (char*)(sc->str);
+  while (pos && p[pos] == ' ') {
+    pos--;
+  }
+  if (pos == 0 || p[pos] == '\n') {
+    size_t indent = sc->pos - 1 - pos;
+    if (sc->last_indent == -1) {
+      sc->last_indent = indent;
+      return indent ? INDENT : SAMEDENT;
+    } else {
+      int r = (sc->last_indent < indent ? INDENT : sc->last_indent > indent ? DEDENT : SAMEDENT);
+      sc->last_indent = indent;
+      return r;
+    }
+  } else {
+    return NOTDENT;
+  }
+}
+
 // prereq: ty is one of [ a A z Z b B ]
 // returns TRUE (match) or FALSE (not match)
-static int scanner_scan_special(Scanner* sc, char ty) {
+static bool scanner_scan_special(Scanner* sc, char ty) {
   switch (ty) {
     case 'a': // line start
       return !sc->pos || sc->str[sc->pos - 1] == '\n';
@@ -132,5 +168,5 @@ static int scanner_scan_special(Scanner* sc, char ty) {
       return !((sc->pos && scanner_word_boundary(sc, sc->pos - 1)) ||
         (!scanner_eos(sc) && scanner_word_boundary(sc, sc->pos)));
   }
-  return 0;
+  return false;
 }
